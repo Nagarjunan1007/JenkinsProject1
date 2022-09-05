3fc647bab48ec1f6e5eb485c37ea19e10c931d2c
Nagarjunan1007 <109644961+Nagarjunan1007@users.noreply.github.com>
2022-09-05 20:06:30 +0530

    Add files via upload
    Initial Commit

---
+++ JenkinsProject.java
@@ -0,0 +1,12 @@
+public class JenkinsProject {
+
+ public static void main(String[] args) {
+  // TODO Auto-generated method stub
+    System.out.println("Hello Arjun");
+	System.out.println("Hello Gowtham");
+	System.out.println("Hello Automation");
+	System.out.println("Hello Verizon");
+
+ }
+
+}
