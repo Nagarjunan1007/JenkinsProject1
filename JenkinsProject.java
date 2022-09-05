3ee93cc2df120da57c64831ad7fbf21621680c20
Nagarjunan1007 <109644961+Nagarjunan1007@users.noreply.github.com>
2022-09-05 20:56:23 +0530

    Update JenkinsProject.java

--- JenkinsProject.java
+++ JenkinsProject.java
@@ -1,4 +1,4 @@
-public class JenkinsProject1 {
+public class JenkinsProject{
 
  public static void main(String[] args) {
   // TODO Auto-generated method stub
@@ -9,7 +9,7 @@ public class JenkinsProject1 {
         System.out.println("Hello TestAutomation12");
 	 System.out.println("Hello TestAutomation1212334");
 	  System.out.println("Arjun Test1234456");
-	 	  System.out.println("Gowtham Test12342323");
+	 	  System.out.println("Gowtham Test123423233435");
  }
 
 }
